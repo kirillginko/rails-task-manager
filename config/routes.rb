cbda240d35b91143f175ef004bcc55a111bec0a6
kirill ginko <kirillginko@gmail.com>
2020-01-27 11:34:08 -0500

    task-manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  # frozen_string_literal: true
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'tasks' => 'tasks#index', as: :tasks
-  get 'tasks/new' => 'tasks#new', as: :tasks_new
-  get 'tasks/:id' => 'tasks#show', as: :task
-  post 'tasks' => 'tasks#create'
-  get 'tasks/:id/edit' => 'tasks#edit', as: :task_edit
-  patch 'tasks/:id' => 'tasks#update'
-  delete 'tasks/:id' => 'tasks#destroy'
-
-
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root to: 'tasks#index'
+  # Display all tasks
+  # get 'tasks', to: 'tasks#index'
+  # Add a new post
+  # get 'task/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+  # Display one task
+  # get 'task/:id', to: 'tasks#show', as: :task
+  # Update a task
+  # get 'task/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'task/:id', to: 'tasks#update'
+  # Delete a Task
+  # delete 'task/:id', to: 'tasks#destroy',
+  resources :tasks
 end
